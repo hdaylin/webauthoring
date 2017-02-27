f5cdbf8442d08ad2fc0093b1aadc6573d2018b5c
hdaylin <hdaylin@yahoo.com>
2017-02-27 16:15:52 -0500

    added python

---
+++ python /ex2.py
@@ -0,0 +1,9 @@
+fname = "Bob"
+lname = "Newhard"
+pay_rate= 25.25 
+hours_worked = 45 
+total = pay_rate * hours_worked 
+
+print "Employee: ", fname, lname, "has worked ", hours_worked, "at ", pay_rate, "for a total of ", total 
+
+
